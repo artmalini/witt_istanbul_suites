14d7bd08686b61be4366a6f3df247aa7b936adc7
Artem <artmalini@gmail.com>
2019-08-10 14:35:54 +0300

    status1

--- src/Contentful.js
+++ src/Contentful.js
@@ -3,6 +3,6 @@ import { createClient } from 'contentful';
 //const contentful = require("contentful");
 
 export default createClient({
-  space: 'tt02c81h406v',
-  accessToken: 'VBmxPRhbU8WD9dXlMDnlKDs9TItEswAIeSQZmSn_rnk'
+  space: process.env.REACT_APP_API_SPACE,
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN
 });
